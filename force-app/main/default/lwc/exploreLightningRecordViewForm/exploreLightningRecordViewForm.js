42afe6e5a0e1421486910a0e27aadecfda29e709
Salesforce Casts <teja@salesforcecasts.com>
2021-05-19 17:47:04 +0530

    Update exploreLightningRecordViewForm.js

--- force-app/main/default/lwc/exploreLightningRecordViewForm/exploreLightningRecordViewForm.js
+++ force-app/main/default/lwc/exploreLightningRecordViewForm/exploreLightningRecordViewForm.js
@@ -5,4 +5,32 @@ export default class ExploreLightningRecordViewForm extends LightningElement {
   recordId;
 
   objectApiName = "Account";
+  
+  handleSubmit(event) {
+    console.log("IN HANDLE SUBMIT");
+
+    //1. stop the form from submitting
+    event.preventDefault();
+
+    //2. Update a few fields
+    const fields = event.detail.fields;
+    fields.Rating = "Cold";
+
+    //3. submit the form
+    this.template.querySelector("lightning-record-edit-form").submit(fields);
+  }
+
+  handleSuccess() {
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title: "Success",
+        message: "Record is saved!!!",
+        variant: "success"
+      })
+    );
+  }
+
+  handleError() {
+    console.log("IN HANDLE ERROR");
+  }
 }
