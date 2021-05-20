df60f5b0e281402be00c4d89d03e03d660de0aa3
Salesforce Casts <teja@salesforcecasts.com>
2021-05-20 15:15:47 +0530

    Add files via upload

---
+++ force-app/main/default/lwc/exploreWireProp/exploreWireProp.js
@@ -0,0 +1,23 @@
+import { LightningElement, wire } from "lwc";
+
+import fetchRecords from "@salesforce/apex/ExploreWireController.fetchRecords";
+import getUsername from "@salesforce/apex/ExploreWireController.getUsername";
+import processAccounts from "@salesforce/apex/ExploreWireController.processAccounts";
+
+export default class ExploreWireProp extends LightningElement {
+  @wire(fetchRecords)
+  accounts;
+
+  @wire(getUsername)
+  username({ error, data }) {
+    if (data) {
+      console.log(data);
+    } else {
+      console.log(error);
+    }
+  }
+
+  handleClick() {
+    processAccounts();
+  }
+}
