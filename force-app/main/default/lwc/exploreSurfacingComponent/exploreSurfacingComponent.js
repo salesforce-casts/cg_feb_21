3350bc748039d75120860688713381ac99a25708
Salesforce Casts <teja@salesforcecasts.com>
2021-06-01 12:58:18 +0530

    Update exploreSurfacingComponent.js

--- force-app/main/default/lwc/exploreSurfacingComponent/exploreSurfacingComponent.js
+++ force-app/main/default/lwc/exploreSurfacingComponent/exploreSurfacingComponent.js
@@ -2,5 +2,8 @@ import { LightningElement, api } from "lwc";
 
 export default class ExploreSurfacingComponent extends LightningElement {
   @api
-  prop1;
+  noOfMonths;
+  
+  @api
+  loanAmount;
 }
