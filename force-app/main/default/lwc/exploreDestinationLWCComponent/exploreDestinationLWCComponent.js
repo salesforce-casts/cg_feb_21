3a779f7937356f8ba29f6bc779a83a1a01da41ff
Salesforce Casts <teja@salesforcecasts.com>
2021-06-01 11:04:00 +0530

    Update exploreDestinationLWCComponent.js

--- force-app/main/default/lwc/exploreDestinationLWCComponent/exploreDestinationLWCComponent.js
+++ force-app/main/default/lwc/exploreDestinationLWCComponent/exploreDestinationLWCComponent.js
@@ -1,3 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class ExploreDestinationLWCComponent extends LightningElement {}
+export default class ExploreDestinationLWCComponent extends LightningElement {
+  @api
+  m;
+}
